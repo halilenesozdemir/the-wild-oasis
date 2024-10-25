e35981b32ba84aa0942f04909ef4f0c8fde21f53
halilenesozdemir <halil.enes.ozdemir@gmail.com>
2024-10-25 23:55:52 +0300

    feat:Building More Reusable Styled Components part completed.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { GlobalStyles } from "./styles/GlobalStyles";
 import { Button } from "./ui/Button";
 import { Input } from "./ui/Input";
 import { Heading } from "./ui/Heading";
+import { Row } from "./ui/Row";
 
 const StyledApp = styled.main`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -23,19 +24,36 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Heading as="h2">Check in and Out </Heading>
-        <Button onClick={() => alert("check in")}>Check in</Button>
-        <Button onClick={() => alert("check out")}>Check out</Button>
-        {/* <Button>Click me</Button>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and Out </Heading>
+              <Button onClick={() => alert("check in")}>Check in</Button>
+              <Button
+                variation="danger"
+                size="large"
+                onClick={() => alert("check out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+          {/* <Button>Click me</Button>
         <Button primary>Click me</Button>
         <Button>Halil Enes Özdemir</Button>
         <Button as={ReversedButton} primary>
           Halil Enes Özdemir
         </Button>
         <GreenButton>Hellü</GreenButton> */}
-        <Heading as="h3">Forms</Heading>
-        <Input type="number" placeholder="Number of guests" />
+          <Row>
+            <Heading as="h3">Forms</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
